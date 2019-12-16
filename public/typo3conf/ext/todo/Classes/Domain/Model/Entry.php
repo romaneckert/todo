461e46f4617c9e2b7762c75f6e0bcf55afef5e8a
Roman Eckert <mail@romaneckert.de>
2019-12-16 19:37:33 +0100

    update EntryRepository / EntryModel

--- public/typo3conf/ext/todo/Classes/Domain/Model/Entry.php
+++ public/typo3conf/ext/todo/Classes/Domain/Model/Entry.php
@@ -1,116 +1,43 @@
 <?php
+
 namespace Eckert\Todo\Domain\Model;
 
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 
-/***************************************************************
- *
- *  Copyright notice
- *
- *  (c) 2015 Roman Eckert <mail@romaneckert.de>
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+class Entry extends AbstractEntity {
 
-/**
- * Entry
- */
-class Entry extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+    /** @var string */
+    protected $title = '';
 
-	/**
-	 * title
-	 *
-	 * @var string
-	 */
-	protected $title = '';
+    /** @var boolean */
+    protected $solved = 0;
 
-	/**
-	 * solved
-	 *
-	 * @var integer
-	 */
-	protected $solved = 0;
+    /** @var boolean */
+    protected $deleted = 0;
 
-	/**
-	 * deleted
-	 *
-	 * @var integer
-	 */
-	protected $deleted = 0;
+    public function getTitle(): string {
+        return $this->title;
+    }
 
-	/**
-	 * Returns the title
-	 *
-	 * @return string $title
-	 */
-	public function getTitle() {
-		return $this->title;
-	}
+    public function setTitle(string $title): void {
+        $this->title = $title;
+    }
 
-	/**
-	 * Sets the title
-	 *
-	 * @param string $title
-	 * @return void
-	 */
-	public function setTitle($title) {
-		$this->title = $title;
-	}
+    public function isSolved(): bool {
+        return $this->solved;
+    }
 
-	/**
-	 * Returns the solved
-	 *
-	 * @return integer $solved
-	 */
-	public function getSolved() {
+    public function setSolved(bool $solved): void {
+        $this->solved = $solved;
+    }
 
-		return $this->solved;
-	}
+    public function getDeleted(): bool {
+        return $this->deleted;
+    }
 
-	/**
-	 * Sets the solved
-	 *
-	 * @param integer $solved
-	 * @return void
-	 */
-	public function setSolved($solved) {
-		$this->solved = $solved;
-	}
-
-	/**
-	 * Returns the deleted
-	 *
-	 * @return integer $deleted
-	 */
-	public function getDeleted() {
-
-		return $this->deleted;
-	}
-
-	/**
-	 * Sets the deleted
-	 *
-	 * @param integer $deleted
-	 * @return void
-	 */
-	public function setDeleted($deleted) {
-		$this->deleted = $deleted;
-	}
+    public function setDeleted(bool $deleted): void {
+        $this->deleted = $deleted;
+    }
 
 
 }
