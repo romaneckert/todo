db22d86681bc793f2e10fa79bd3b29bc14713b41
Roman Eckert <mail@romaneckert.de>
2019-12-16 19:35:31 +0100

    migrate ext_tables to tt_content.php / update tca and sql

---
+++ public/typo3conf/ext/todo/Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Eckert.Todo',
+    'List',
+    'TODO::List'
+);
---
+++ public/typo3conf/ext/todo/Configuration/TCA/tx_todo_domain_model_entry.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'ctrl' => [
+        'title' => 'Entry',
+        'label' => 'title',
+    ],
+    'columns' => [
+        'title' => [
+            'exclude' => 1,
+            'label' => 'Title',
+            'config' => [
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'trim'
+            ],
+        ],
+        'solved' => [
+            'exclude' => 1,
+            'label' => 'Solved',
+            'config' => [
+                'type' => 'check',
+                'default' => 0
+            ]
+        ],
+        'deleted' => [
+            'exclude' => 1,
+            'label' => 'Deleted',
+            'config' => [
+                'type' => 'check',
+                'default' => 0
+            ]
+        ],
+    ],
+    'types' => [
+        '0' => ['showitem' => 'title,solved,deleted']
+    ]
+];
